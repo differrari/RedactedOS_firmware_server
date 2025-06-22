d07c0602b2d0ac5382ef84b3990f2b6dc5d521b6
Diego Ferrari <dieferhau@me.com>
2025-06-22 00:00:00 +0000

    Test json

--- http.js
+++ http.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
+var payload = {
+  content: [
+    {
+      "name" : "test.elf",
+      "size": 1,
+      "version" : 0x000
+    },
+    {
+      "name" : "firmware.elf",
+      "size": 1,
+      "version" : 0x002
+    },
+    {
+      "name" : "game.elf",
+      "size": 100,
+      "version" : 0x010
+    },
+  ],
+  len: 3
+}
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello world');
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(payload));
 });
 
 server.listen(80, () => {
